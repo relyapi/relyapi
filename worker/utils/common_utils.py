5924eb55641d66bcd5349202574198bc494ff8e6
gaozhe <gaozhe@baiwang.com>
2025-07-23 21:41:06 +0800

    feat: delploy

--- worker/utils/common_utils.py
+++ worker/utils/common_utils.py
@@ -1,12 +1,24 @@
 import hashlib
 
+import httpx
 import tldextract
-from tenacity import retry, stop_after_attempt, wait_exponential
+from tenacity import retry, stop_after_attempt, wait_exponential, wait_fixed
 
 retry_strategy = retry(
     stop=stop_after_attempt(3),
     wait=wait_exponential(multiplier=1, min=4, max=10)
 )
+
+
+@retry(
+    reraise=True,  # 保留原始异常
+    stop=stop_after_attempt(3),  # 最多重试 3 次
+    wait=wait_fixed(1),  # 每次失败后等待 2 秒
+    # retry=retry_if_exception_type(httpx.RequestError)  # 只在网络异常时重试
+)
+async def fetch_with_retry(client: httpx.AsyncClient, result: dict):
+    resp = await client.request(**result)
+    return resp
 
 
 def extract_main_domain(host: str) -> str:
